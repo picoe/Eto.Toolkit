df9d817838bd38e4e99bdb0aaa633cafc1476717
Steve Baer <steve@mcneel.com>
2018-02-16 17:53:38 -0800

    attempting to create binding project for scintilla

---
+++ src/Scintilla.XamMac2/ApiDefinition.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using AppKit;
+using CoreGraphics;
+using Foundation;
+using ObjCRuntime;
+//using Scintilla;
+
+
+// @interface ScintillaView : NSView <InfoBarCommunicator, ScintillaNotificationProtocol>
+[BaseType(typeof(NSView))]
+interface ScintillaView 
+{
+  // @property (assign, nonatomic) id<ScintillaNotificationProtocol> delegate;
+  [NullAllowed, Export("delegate", ArgumentSemantic.Assign)]
+  NSObject WeakDelegate { get; set; }
+
+  // @property (readonly, nonatomic) NSScrollView * scrollView;
+  [Export("scrollView")]
+  NSScrollView ScrollView { get; }
+
+
+  // -(void)setGeneralProperty:(int)property parameter:(long)parameter value:(long)value;
+  [Export("setGeneralProperty:parameter:value:")]
+  void SetGeneralProperty(int property, nint parameter, nint value);
+
+  // -(void)setGeneralProperty:(int)property value:(long)value;
+  [Export("setGeneralProperty:value:")]
+  void SetGeneralProperty(int property, nint value);
+
+  // -(long)getGeneralProperty:(int)property;
+  [Export("getGeneralProperty:")]
+  nint GetGeneralProperty(int property);
+
+  // -(long)getGeneralProperty:(int)property parameter:(long)parameter;
+  [Export("getGeneralProperty:parameter:")]
+  nint GetGeneralProperty(int property, nint parameter);
+
+  // -(long)getGeneralProperty:(int)property parameter:(long)parameter extra:(long)extra;
+  [Export("getGeneralProperty:parameter:extra:")]
+  nint GetGeneralProperty(int property, nint parameter, nint extra);
+
+  // -(void)setColorProperty:(int)property parameter:(long)parameter value:(NSColor *)value;
+  [Export("setColorProperty:parameter:value:")]
+  void SetColorProperty(int property, nint parameter, NSColor value);
+
+  // -(void)setColorProperty:(int)property parameter:(long)parameter fromHTML:(NSString *)fromHTML;
+  [Export("setColorProperty:parameter:fromHTML:")]
+  void SetColorProperty(int property, nint parameter, string fromHTML);
+
+  // -(NSColor *)getColorProperty:(int)property parameter:(long)parameter;
+  [Export("getColorProperty:parameter:")]
+  NSColor GetColorProperty(int property, nint parameter);
+}
---
+++ src/Scintilla.XamMac2/StructsAndEnums.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using ObjCRuntime;
+//using Scintilla;
+
+static class CFunctions
+{
+  // extern int Scintilla_LinkLexers ();
+  [DllImport ("__Internal")]
+  //[Verify (PlatformInvoke)]
+  static extern int Scintilla_LinkLexers ();
+}
+
+[StructLayout (LayoutKind.Sequential)]
+public struct Sci_CharacterRange
+{
+  public nint cpMin;
+
+  public nint cpMax;
+}
+
+[StructLayout (LayoutKind.Sequential)]
+public struct Sci_TextRange
+{
+  public Sci_CharacterRange chrg;
+
+  public unsafe sbyte* lpstrText;
+}
+
+[StructLayout (LayoutKind.Sequential)]
+public struct Sci_TextToFind
+{
+  public Sci_CharacterRange chrg;
+
+  public unsafe sbyte* lpstrText;
+
+  public Sci_CharacterRange chrgText;
+}
+
+[StructLayout (LayoutKind.Sequential)]
+public struct Sci_Rectangle
+{
+  public int left;
+
+  public int top;
+
+  public int right;
+
+  public int bottom;
+}
+
+[StructLayout (LayoutKind.Sequential)]
+public struct Sci_RangeToFormat
+{
+  public unsafe void* hdc;
+
+  public unsafe void* hdcTarget;
+
+  public Sci_Rectangle rc;
+
+  public Sci_Rectangle rcPage;
+
+  public Sci_CharacterRange chrg;
+}
+
+[StructLayout (LayoutKind.Sequential)]
+public struct Sci_NotifyHeader
+{
+  public unsafe void* hwndFrom;
+
+  public UIntPtr idFrom;
+
+  public uint code;
+}
+
+[StructLayout (LayoutKind.Sequential)]
+public struct SCNotification
+{
+  public Sci_NotifyHeader nmhdr;
+
+  public int position;
+
+  public int ch;
+
+  public int modifiers;
+
+  public int modificationType;
+
+  public unsafe sbyte* text;
+
+  public int length;
+
+  public int linesAdded;
+
+  public int message;
+
+  public UIntPtr wParam;
+
+  public IntPtr lParam;
+
+  public int line;
+
+  public int foldLevelNow;
+
+  public int foldLevelPrev;
+
+  public int margin;
+
+  public int listType;
+
+  public int x;
+
+  public int y;
+
+  public int token;
+
+  public int annotationLinesAdded;
+
+  public int updated;
+
+  public int listCompletionMethod;
+}
